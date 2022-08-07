b784d94b8c581fa3f0ede51c0ed096ad9a34546c
musaddekali <musaddekali.dev@gmail.com>
2022-08-07 11:36:39 +0600

    header scrolled shrink option added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import './Header.css';
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(null);
+
+  const headerScrolled = () => {
+    window.addEventListener('scroll', () => {
+      const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+      if (winScroll > 80) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+
+      if (winScroll > 80) {
+        setIsScrolled(true);
+      }
+    })
+  }
+
+  useEffect(() => {
+    headerScrolled();
+    return () => window.removeEventListener('scroll', headerScrolled);
+  }, []);
+
   return (
-    <header className='header'>
+    <header className={`header ${isScrolled ? 'header-scrolled' : ''}`}>
       <div className="container-fluid">
         <Navbar />
       </div>
